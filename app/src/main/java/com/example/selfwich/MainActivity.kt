ccc0649d3fa786b2ed905be15a29c88f2c5ef17d
Yussha <ymertcakir@gmail.com>
2021-06-19 12:56:43 +0300

    Drawer menu implemented

--- app/src/main/java/com/example/selfwich/MainActivity.kt
+++ app/src/main/java/com/example/selfwich/MainActivity.kt
@@ -3,20 +3,36 @@ package com.example.selfwich
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.selfwich.R.id
 import com.example.selfwich.R.id.my_navFragment
 import com.example.selfwich.R.id.my_nav_host
+import com.example.selfwich.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var drawerLayout: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
 
+        drawerLayout=findViewById(R.id.drawerLayout)
+        val navView = findViewById<NavigationView>(R.id.navView)
+
         val navController = findNavController(my_navFragment)
+
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(navView,navController)
+
+
+
 
         val bottomNavigationView = findViewById<BottomNavigationView>(id.bottom_navbar)
 
